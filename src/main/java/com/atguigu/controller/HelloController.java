7056bf48f3f046b08aca0ab8bffb7be80dcbf08d
tom_glb <goodMorning_glb@atguigu.com>
2019-08-22 11:16:31 +0800

    第一次提交

---
+++ src/main/java/com/atguigu/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.atguigu.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//这个类的所有的方法返回的数据直接写给浏览器（如果是对象转为json数据）
+// @ResponseBody
+// @Controller
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world quick";
+    }
+}
